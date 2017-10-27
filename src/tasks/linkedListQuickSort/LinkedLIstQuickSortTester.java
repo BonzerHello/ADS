1987d945fd1939ddee9f113b53f23d4451ff7742
Tim <tim.kriemler@itds.ch>
2017-10-27 14:14:36 +0200

    quicksort

--- src/tasks/linkedListQuickSort/LinkedLIstQuickSortTester.java
+++ src/tasks/linkedListQuickSort/LinkedLIstQuickSortTester.java
@@ -1,18 +1,20 @@
-package tasks.linkedLIstMergeSortTester;
+package tasks.linkedListQuickSort;
 
 import goldman.collection.positional.SinglyLinkedList;
 
-public class LinkedListMergeSortTester {
-
+public class LinkedLIstQuickSortTester {
     public static void main(String[] args){
         SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
         list.add(11);
         list.add(4);
+        list.add(9);
+        list.add(7);
         list.add(3);
         list.add(10);
         list.add(2);
+        list.add(6);
         System.out.println("Vorher: "+list.toString());
-        list.mergesort();
+        list.quicksort();
         System.out.println("Nachher: "+list.toString());
     }
 }
